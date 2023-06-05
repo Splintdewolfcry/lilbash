5e4fe41d1ee7d72f8cce443e1288e6e4164ccd4d
Splintdewolfcry <spymaccry@gmail.com>
2023-06-05 21:27:36 +0000

    Create toberun.sh
    First Draft

---
+++ toberun.sh
@@ -0,0 +1,8 @@
+# Some commands I'd like my machine to run as soon as it's avaliable:
+
+sudo apt update
+sudo apt upgrade
+sudo apt install --assume-yes snap
+sudo snap install ngrok
+wget -qO- https://sitejs.org/install | bash
+site enable --owncast
